7e70fe330d3243389fc49444c756f6d0cf7344da
박준현 <108358152+Wd2wad3@users.noreply.github.com>
2023-03-31 08:26:19 +0900

    Initial Commit

---
+++ SwUICal/SwUICalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUICalApp.swift
+//  SwUICal
+//
+//  Created by Hyun on 2023/03/31.
+//
+
+import SwiftUI
+
+@main
+struct SwUICalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
